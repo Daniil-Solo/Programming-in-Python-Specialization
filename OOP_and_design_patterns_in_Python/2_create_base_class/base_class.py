8f810736851c365a486696b28f3d84a3a657ecd9
Daniil-Solo <daniil.solo1723@gmail.com>
2021-08-22 15:39:21 +0300

    add base_class with pass-methods

---
+++ OOP_and_design_patterns_in_Python/2_create_base_class/base_class.py
@@ -0,0 +1,13 @@
+class Base:
+    def __init__(self, data, result):
+        self.data = data
+        self.result = result
+
+    def get_answer(self):
+        pass
+
+    def get_loss(self):
+        pass
+
+    def get_score(self):
+        pass
