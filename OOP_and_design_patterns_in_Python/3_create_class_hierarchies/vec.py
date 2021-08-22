210596ac5d0f0d00f7a592d1175033552aeb5e27
Daniil-Solo <daniil.solo1723@gmail.com>
2021-08-22 20:18:06 +0300

    add class Vec2d

---
+++ OOP_and_design_patterns_in_Python/3_create_class_hierarchies/vec.py
@@ -0,0 +1,22 @@
+import math
+
+
+class Vec2d:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vec2d(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vec2d(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Vec2d(self.x * other, self.y * other)
+
+    def __len__(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def int_pair(self):
+        return self.x, self.y
