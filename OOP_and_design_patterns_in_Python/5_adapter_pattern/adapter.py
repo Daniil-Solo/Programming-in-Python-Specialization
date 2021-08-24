922b75cce3f3e24c6c2ee7390777da40d0bb0f87
Daniil-Solo <daniil.solo1723@gmail.com>
2021-08-24 21:37:24 +0300

    add adapter functions

--- OOP_and_design_patterns_in_Python/5_adapter_pattern/adapter.py
+++ OOP_and_design_patterns_in_Python/5_adapter_pattern/adapter.py
@@ -1,6 +1,24 @@
 class MappingAdapter:
     def __init__(self, adaptee):
-        pass
+        self.adaptee = adaptee
+
+    def _get_lights_and_obstacles(self, grid):
+        lights = []
+        obstacles = []
+        for i in range(self.adaptee.dim[0]):
+            for j in range(self.adaptee.dim[1]):
+                if grid[i][j] == 1:
+                    lights.append((i, j))
+                elif grid[i][j] == -1:
+                    obstacles.append((i, j))
+                else:
+                    continue
+        return lights, obstacles
 
     def lighten(self, grid):
-        pass
+        dim = (len(grid[0]), len(grid)) # т.к. класс применяет сначала ширину, а потом высоту
+        self.adaptee.set_dim(dim)
+        lights, obstacles = self._get_lights_and_obstacles(grid)
+        self.adaptee.set_obstacles(obstacles)
+        self.adaptee.set_lights(lights)
+        return self.adaptee.generate_lights()
