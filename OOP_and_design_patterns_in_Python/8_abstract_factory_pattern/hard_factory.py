a3a266de386c3fcb8f89e6d892558d2e3da61b47
Daniil-Solo <daniil.solo1723@gmail.com>
2021-08-26 19:50:53 +0300

    add all game factories

---
+++ OOP_and_design_patterns_in_Python/8_abstract_factory_pattern/hard_factory.py
@@ -0,0 +1,45 @@
+import random
+from abstract_factory import AbstractLevel
+
+
+class EasyLevel(AbstractLevel):
+    class Map:
+        def __init__(self):
+            self._map = [[0 for j in range(10)] for i in range(10)]
+            for i in range(10):
+                for j in range(10):
+                    if i == 0 or j == 0 or i == 9 or j == 9:
+                        # граница карты
+                        self._map[j][i] = -1
+                    else:
+                        # характеристика области (-1 для непроходимой обл.)
+                        self._map[j][i] = random.randint(-1, 8)
+
+        def get_map(self):
+            return self._map
+
+    class Objects:
+        def __init__(self):
+            # размещаем переход на след. уровень
+            self.objects = [('next_lvl', (5, 5))]
+
+        def get_objects(self, map_obj):
+            # размещаем врагов
+            for obj_name in ['rat', 'snake']:
+                coord = (random.randint(1, 8), random.randint(1, 8))
+                # ищем случайную свободную локацию
+                intersect = True
+                while intersect:
+                    intersect = False
+                    if map_obj[coord[0]][coord[1]] == -1:
+                        intersect = True
+                        coord = (random.randint(1, 8), random.randint(1, 8))
+                        continue
+                    for obj in self.objects:
+                        if coord == obj[1]:
+                            intersect = True
+                            coord = (random.randint(1, 8), random.randint(1, 8))
+
+                self.objects.append((obj_name, coord))
+
+            return self.objects
